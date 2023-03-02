490fdf2c1cd4e720ef31e5dfe1a07b2d7e3142a5
Ty <ty@Tys-MacBook-Air.local>
2023-03-01 22:33:53 -0600

    　

--- pyntrude.py
+++ pyntrude.py
@@ -3,12 +3,11 @@ import sys
 import urllib3
 
 #########  Usage: python3 pyntrude.py <request_file> <payload_file> <error_keyword>  ##########
+######### Example: python3 pyntrude.py req.txt /usr/share/wordlists/seclists/Passwords/Common-Credentials/best1050.txt Invalid #########
 urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
 
+#Proxy set if you want to intercept request in burp
 proxies = {'http': 'http://127.0.0.1:8080', 'https': 'http://127.0.0.1:8080'}
-
-def parse_req(file):
-    print()
 
 def intrude(s):
     filename = sys.argv[1]
@@ -23,50 +22,44 @@ def intrude(s):
         for line in file:
             
             # Set the current line as a string
-      
             current_line = line.rstrip()
             with open(filename, 'r') as file:
                 file_contents = file.read()
                 lines = file_contents.splitlines()
-
+                
+	    #find where the fuzzing parameter is so we can replace it with the current payload
             first_index = file_contents.find('§')
             last_index = file_contents.rfind('§')
 
             if first_index != -1 and last_index != -1:
                 file_contents = file_contents[:first_index] + current_line + file_contents[last_index+1:]
 
-            #print(file_contents)
-
-
+	    #split headers and body into respective variables
             headersraw, body = file_contents.split('\n\n', 1)
 
-            #print('Headers:')
-            #print(headersraw)
-            #print('Body:')
-            #print(body)
-
-
+	    #create dictionary for the headers
             headers = {}
             for line in headersraw.splitlines():
                 if ':' in line:
                     key, value = line.split(':', 1)
                     headers[key.strip()] = value.strip()
 
+	    #craft the URL using the Origin header and given endpoint
             url = headers["Origin"]
             endpoint = headersraw.split()[1]
             target = url + endpoint
-            #Set the request payload in the request:
+
 
             #Craft the request
             r = s.post(target, headers=headers, data=body, verify=False, proxies=proxies)
-
             res = r.text
-            #print(res)
 
+	    #return success or fail based on the response
             if error not in res:
                 print("(+) Success with payload: " + current_line )
-
-
+            else:
+            	print("(-) Failed with payload: " + current_line )
+            	
 
 
 
@@ -79,7 +72,6 @@ def main():
 
 
 
-    
 
 if __name__ == "__main__":
     main()
